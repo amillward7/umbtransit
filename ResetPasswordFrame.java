fd45d0f6b06a8c690171e547e6bdebc1ce310fed
danielgarrard001 <daniel.garrard001@umb.edu>
2017-04-25 13:12:47 -0400

    Add files via upload

---
+++ ResetPasswordFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ResetPasswordFrame extends JFrame {
+  private JPanel p;
+  
+  public ResetPasswordFrame() {
+    super();
+    p = new ResetPassword();
+    add(p);
+    pack();
+    setBounds(300, 150, 450, 750);
+    setVisible(true);
+  }
+}

f7037fc04685d1f23e8eff4165305ba5acd30108
danielgarrard001 <daniel.garrard001@umb.edu>
2017-04-25 13:40:32 -0400

    Added links to buttons

--- login.java
+++ login.java
@@ -1,38 +1,61 @@
-package parkinglots;
+//package parkinglots;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class login {
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Login");
-		frame.setBounds(300, 150, 450, 750);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.anchor = GridBagConstraints.WEST;
-		
-		JLabel usernameLabel = new JLabel("<html>Username:  <br></html>");
-		JLabel passwordLabel = new JLabel("<html>Password:  <br></html>");
-		JTextField usernameText = new JTextField(15);
-		JTextField passwordText = new JTextField(15);
-		JButton submit = new JButton("Log in");
-		add(frame, usernameLabel, c, 0, 2, 1, 1);
-		add(frame, passwordLabel, c, 0, 4, 1, 1);
-		add(frame, usernameText, c, 2, 2, 1, 1);
-		add(frame, passwordText, c, 2, 4, 1, 1);
-		add(frame, submit, c, 0, 6, 1, 1);
-		
+ public static void main(String[] args) {
+  final JFrame frame = new JFrame("Login");
+  frame.setBounds(300, 150, 450, 750);
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  
+  frame.setLayout(new GridBagLayout());
+  GridBagConstraints c = new GridBagConstraints();
+  c.anchor = GridBagConstraints.WEST;
+  
+  JLabel usernameLabel = new JLabel("<html>Username:  <br></html>");
+  JLabel passwordLabel = new JLabel("<html>Password:  <br></html>");
+  JTextField usernameText = new JTextField(15);
+  JTextField passwordText = new JTextField(15);
+  JButton submit = new JButton("Log in");
+  submit.addActionListener(new ActionListener() {
+    public void actionPerformed(ActionEvent e) {
+      new ParkingInfoPage();
+      frame.dispose();
+    }
+  });
+  JButton register = new JButton("Register");
+  register.addActionListener(new ActionListener() {
+    public void actionPerformed(ActionEvent e) {
+      new RegisterPage();
+      frame.dispose();
+    }
+  });
+  JButton forgetpw = new JButton("Forget Password");
+  forgetpw.addActionListener(new ActionListener() {
+    public void actionPerformed(ActionEvent e) {
+      new ResetPasswordFrame();
+      frame.dispose();
+    }
+  });
+  add(frame, usernameLabel, c, 0, 2, 1, 1);
+  add(frame, passwordLabel, c, 0, 4, 1, 1);
+  add(frame, usernameText, c, 2, 2, 1, 1);
+  add(frame, passwordText, c, 2, 4, 1, 1);
+  add(frame, submit, c, 0, 6, 1, 1);
+  add(frame, forgetpw, c, 2, 6, 1, 1);
+  add(frame, register, c, 0, 8, 1, 1);
+  
         frame.setVisible(true);
-	}
-	
-	public static void add(JFrame frame, Component label, GridBagConstraints c, int x, int y, int w, int h){
-		c.gridx = x;
-		c.gridy = y;
-		c.gridwidth = w;
-		c.gridheight = h;
-		frame.add(label, c);
-	}
+ }
+ 
+ public static void add(JFrame frame, Component label, GridBagConstraints c, int x, int y, int w, int h){
+  c.gridx = x;
+  c.gridy = y;
+  c.gridwidth = w;
+  c.gridheight = h;
+  frame.add(label, c);
+ }
 
 }
